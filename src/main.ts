31244dc44dfd1037cb44ed3de32bd994733ea812
gstrainovic <g.strainovic@gmail.com>
2023-07-04 22:07:58 +0200

    before prices fix

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,47 @@
 import { PlaywrightCrawler } from 'crawlee';
 
+type Data = {
+    title: string
+    prices: { quantity: string; price: string }[];
+    sku: string;
+    pcsPalette: string;
+}
+
+const data = [] as Data[];
+
 const crawler = new PlaywrightCrawler({
     requestHandler: async ({ page }) => {
         await page.waitForSelector('h2.product_title.entry-title');
-        const actorTexts = await page.$$eval('h2.product_title.entry-title', (els) => {
-            return els.map((el) => el.textContent);
+        const titleAr = await page.$$eval('h2.product_title.entry-title', (els) => {
+            return els.map((el) => {
+                return el.textContent?.trim();
+            }
+            );
         });
-        actorTexts.forEach((text, i) => {
-            console.log(`H2_${i + 1}: ${text}\n`);
-        });
+        const title = titleAr[0] ?? '';
 
         await page.waitForSelector('div#bulk-price-table table tbody tr');
         const prices = await page.$$eval('div#bulk-price-table table tbody tr', (els) => {
             return els.map((el) => {
-                const quantity = el.querySelector('td:nth-child(1)')?.textContent?.trim();
-                const price = el.querySelector('div.td-2 span + div span')?.textContent?.trim();
+                const quantity = el.querySelector('td:nth-child(1)')?.textContent?.trim() ?? '';
+                const price = el.querySelector('div.td-2 span + div span')?.textContent?.trim() ?? '';
                 return { quantity, price: price };
             });
         });
-        console.log(prices);
 
-        // extract the sku from html like this: <span class="sku_wrapper">Artikelnummer: <span class="sku">24043</span></span>
         await page.waitForSelector('span.sku_wrapper span.sku');
-        const sku = await page.$eval('span.sku_wrapper span.sku', (el) => el.textContent);
-        console.log(`SKU: ${sku}`);
+        const sku = await page.$eval('span.sku_wrapper span.sku', (el) => el.textContent) ?? '';
+
+        await page.waitForSelector('tr.woocommerce-product-attributes-item--attribute_mengepalette td.woocommerce-product-attributes-item__value p');
+        const pcsPalette =
+            await page.$eval('tr.woocommerce-product-attributes-item--attribute_mengepalette td.woocommerce-product-attributes-item__value p',
+                (el) => el.textContent?.trim() ?? '');
+
+        data.push({ title, prices, sku, pcsPalette });
 
     },
 });
 
 await crawler.run(['https://ecoon.de/produkt/graspapierkarton-550-x-300-x-550-350-300-mm-2-wellig/']);
+
+console.log(data);
