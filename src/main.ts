19ee030b4de37a527b0d76b09b2d7fab603437ea
gstrainovic <g.strainovic@gmail.com>
2023-07-10 20:57:39 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { ExportAll, ExportDiff } from './export.js';
 import EcoonScraper from './scraper/ecoon.js';
 import KartonEuScraper from './scraper/karton-eu.js';
 
-const kartonEuScraper = new KartonEuScraper();
-await kartonEuScraper.scrape();
+const kartonEuScraper = new KartonEuScraper()
+await kartonEuScraper.scrape(undefined, true);
 
 const ecoonScraper = new EcoonScraper();
-await ecoonScraper.scrape();
+await ecoonScraper.scrape(undefined, true);
 
 const exportAll = new ExportAll();
 await exportAll.export();
